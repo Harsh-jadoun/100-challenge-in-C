86268a588ab579654ffca6ea822678ecae8aff2e
Harsh kumar jadon <harshgbn07@gmail.com>
2025-05-15 06:16:30 +0530

    Create challenge_008.c

---
+++ 100 C Challenges/challenge_008.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Give two numbers: ");
+    scanf("%d %d",&a,&b);
+    printf("\nBefore swapping a=%d, b=%d",a,b);
+    a = a+b;
+    b= a-b;
+    a= a-b;
+   printf("\nAfter swapping a=%d, b=%d",a,b);
+    return 0;
+}
