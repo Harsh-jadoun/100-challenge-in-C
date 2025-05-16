618e955dc2ac526e9008ed2e919713f45415c524
Harsh kumar jadon <harshgbn07@gmail.com>
2025-05-16 06:05:34 +0530

    Create challenge_011.c

---
+++ 100 C Challenges/challenge_011.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    float cel,far;
+    scanf("%f",&cel);
+    far=(cel*9/5)+32;
+    printf("Temperature in farhenheit is %f",far);
+    return 0;
+}
+
