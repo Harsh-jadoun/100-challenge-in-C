93c7bad66b4676015a3c69712a09d1d4641cf4d7
Harsh kumar jadon <harshgbn07@gmail.com>
2025-05-16 06:06:58 +0530

    Create challenge_012.c

---
+++ 100 C Challenges/challenge_012.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    float cel,far;
+    scanf("%f",&far);
+    cel=(far-32)*9/5;
+    printf("Temperature in Celsius is %f",cel);
+    return 0;
+}
+
