7fefbbd8933c6647f67209330c6d782eafd42a03
Harsh kumar jadon <harshgbn07@gmail.com>
2025-05-21 05:28:19 +0530

    Create challenge_21.c

---
+++ 100 C Challenges/challenge_21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int num;
+    scanf("%d",&num);
+    if(num%2==0)
+    printf("Even");
+    else
+    printf("Odd");
+    return 0;
+}
+
