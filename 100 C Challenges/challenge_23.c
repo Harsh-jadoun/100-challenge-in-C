690d3b2b86f900e1316f604d527e77f4be24a53e
Harsh kumar jadon <harshgbn07@gmail.com>
2025-05-21 05:32:58 +0530

    Create challenge_23.c

---
+++ 100 C Challenges/challenge_23.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    if(num1>num2)
+    printf("%d is maximum",num1);
+    else
+    printf("%d is maximum",num2);
+    return 0;
+}
+
