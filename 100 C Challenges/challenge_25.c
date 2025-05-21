085a582245173b28ca4df88a62c1095d830cb4fa
Harsh kumar jadon <harshgbn07@gmail.com>
2025-05-21 05:36:25 +0530

    Create challenge_25.c

---
+++ 100 C Challenges/challenge_25.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int year;
+    scanf("%d",&year);
+    if(year%4==0)
+    printf("Leap year");
+    else
+    printf("Not a leap year");
+    return 0;
+}
+
