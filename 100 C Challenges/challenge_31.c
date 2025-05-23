3fbc58f8871fbe1e34241eadce35b00301ffecce
Harsh kumar jadon <harshgbn07@gmail.com>
2025-05-23 20:03:45 +0530

    Create challenge_31.c

---
+++ 100 C Challenges/challenge_31.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int num,rem,rev=0;
+	scanf("%d",&num);
+	while(num>0)
+	{
+	    rem=num%10;
+	    rev=rev*10+rem;
+	    num=num/10;
+	}
+	printf("%d",rev);
+	
+	return 0;
+
+}
+
