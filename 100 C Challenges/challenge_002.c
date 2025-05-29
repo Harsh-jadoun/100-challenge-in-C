569d094e584c627069a25d0abe5fc63ee685f850
Harsh kumar jadon <harshgbn07@gmail.com>
2025-05-29 21:26:24 +0530

    Update challenge_002.c

--- 100 C Challenges/challenge_002.c
+++ 100 C Challenges/challenge_002.c
@@ -2,8 +2,8 @@
 int main(){
     int a,b;
     printf("Give two numbers: ");
-    scanf("%d %d",&a,&b);
-    int sum = a+b;
-    printf("\nsum = %d",sum);
+    scanf("%d %d",&a,&b); //Taking numbers as input
+    int sum = a+b; //calculating sum of nummbers
+    printf("\nsum = %d",sum); //Printing sum of numbers
     return 0;
 }
