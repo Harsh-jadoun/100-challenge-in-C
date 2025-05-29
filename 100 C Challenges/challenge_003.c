c79b1cd41109214788b5c3dd4491083a6fb0a66f
Harsh kumar jadon <harshgbn07@gmail.com>
2025-05-29 21:27:25 +0530

    Update challenge_003.c

--- 100 C Challenges/challenge_003.c
+++ 100 C Challenges/challenge_003.c
@@ -3,7 +3,7 @@ int main(){
     int a,b;
     printf("Give two numbers: ");
     scanf("%d %d",&a,&b);
-    int sub = a-b;
-    printf("\nsub = %d",sub);
+    int sub = a-b; //Calculating difference between two numbers
+    printf("\nsub = %d",sub); //Printing difference of two numbers
     return 0;
 }
