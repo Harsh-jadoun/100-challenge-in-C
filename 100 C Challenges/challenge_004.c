6eaa1521a528f3973254e49847ce5db3b462d5e4
Harsh kumar jadon <harshgbn07@gmail.com>
2025-06-02 20:09:45 +0530

    Update challenge_004.c

--- 100 C Challenges/challenge_004.c
+++ 100 C Challenges/challenge_004.c
@@ -2,8 +2,8 @@
 int main(){
     int a,b;
     printf("Give two numbers: ");
-    scanf("%d %d",&a,&b);
-    int mul = a*b;
-    printf("\nmultiply = %d",mul);
+    scanf("%d %d",&a,&b); //Taking input two numbers
+    int mul = a*b;  //Calaculating multiplication
+    printf("\nmultiply = %d",mul); //Printing multiplication
     return 0;
 }
