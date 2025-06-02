48752ed61faf52a4abbecb53a0e9bb5d2e166df4
Harsh kumar jadon <harshgbn07@gmail.com>
2025-06-02 20:12:26 +0530

    Update challenge_007.c

--- 100 C Challenges/challenge_007.c
+++ 100 C Challenges/challenge_007.c
@@ -2,11 +2,12 @@
 int main(){
     int a,b;
     printf("Give two numbers: ");
-    scanf("%d %d",&a,&b);
-    printf("\nBefore swapping a=%d, b=%d",a,b);
-    int c = a;
+    scanf("%d %d",&a,&b); //taking input two numbers
+    printf("\nBefore swapping a=%d, b=%d",a,b); // Beffore swapping values of a and b
+    //Logic of swapping two numbers using third variable
+    int c = a; 
     a = b;
     b = c;
-   printf("\nAfter swapping a=%d, b=%d",a,b);
+   printf("\nAfter swapping a=%d, b=%d",a,b); //After swapping values of a and b
     return 0;
 }
