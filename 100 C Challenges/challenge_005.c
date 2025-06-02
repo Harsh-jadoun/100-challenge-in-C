f2404c3e7d83139d145769e29e11bc25790079f9
Harsh kumar jadon <harshgbn07@gmail.com>
2025-06-02 20:10:28 +0530

    Update challenge_005.c

--- 100 C Challenges/challenge_005.c
+++ 100 C Challenges/challenge_005.c
@@ -2,8 +2,8 @@
 int main(){
     int a,b;
     printf("Give two numbers: ");
-    scanf("%d %d",&a,&b);
-    int div = a/b;
-    printf("\n quotent= %d",div);
+    scanf("%d %d",&a,&b); 
+    int div = a/b; //calculating division
+    printf("\n quotent= %d",div); //printing division
     return 0;
 }
