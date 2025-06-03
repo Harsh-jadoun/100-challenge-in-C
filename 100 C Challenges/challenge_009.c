88490d4afef23c1526c63fe8f882587546703430
Harsh kumar jadon <harshgbn07@gmail.com>
2025-06-03 22:24:13 +0530

    Update challenge_009.c

--- 100 C Challenges/challenge_009.c
+++ 100 C Challenges/challenge_009.c
@@ -2,6 +2,6 @@
 int main(){
    char ch;
    scanf("%c",&ch); //taking character as input
-   printf("ASCII value of %c is %d",ch,ch);
+   printf("ASCII value of %c is %d",ch,ch); //Printing ASCII value of Character
    return 0;
 }
